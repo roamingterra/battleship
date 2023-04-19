5687f397afab39ca856d09c9670c22bc6273323d
roamingterra <danielballerini1@gmail.com>
2023-04-19 16:15:46 -0400

    Write up notes for testing Gameboard factory
    These notes include what is expected of the factory function, and what
    to specifically test and why. I will remove these notes once the tests
    are built.

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { Ship } from "./index";
+import { Ship, GameBoard } from "./index";
 
 // Ship object test fixture
 function shipTestFixture(length, hits) {
@@ -8,7 +8,7 @@ function shipTestFixture(length, hits) {
   }
   return shipInstance;
 }
-// Methods to be tested (related to object public interface)
+// Ship Methods to be tested (related to object public interface)
 // hit() method: Outgoing command method (called from outside the object to change the state of the object)
 // isSunk() method: incoming query method (called from outside the object to return and receive information on the state of the object)
 test("test a hit on a long ship that should not sink", () => {
@@ -30,3 +30,25 @@ test("test several hits on a long ship that should sink", () => {
   // Perform test
   expect(shipInstance.isSunk()).toBe(true);
 });
+
+// GameBoard testing:
+// Factory function that should place ships at specific coordinates by calling Ship factory function
+// .. so gameBoard should contain a 10X10 grid, which should contain the following ships:
+// 1. Carrier: length 5
+// 2. Battleship: length 4
+// 3. Cruiser: length 3
+// 4. Submarine: length 3
+// 5. Patrol Boat: length 2
+// .. so the grid will be represented by a 2D array, which will initially contain values "empty".
+// .. When placing ships, the instances will be created, and with the given coordinates, they will
+// .. be placed in the 2D array as name values (ex: .. "empty", "Cruiser", "Cruiser", "Cruiser", "empty" ..)
+// receiveAttack() method that takes a pair of coordinates, determines if an attack hit a ship, then sends
+// .. hit function to correct ship, or records the coordinates of the missed shot
+// Keep track of missed attacks: by replacing "empty" values in the 2D array with "miss" values
+// Report if all ships have been sunk or not areShipsSunk() method
+
+// What to test?
+// receiveAttack() method is an outgoing command because it gets called from outside the unit to change
+// .. the state of the unit
+// areShipsSunk() method is an incoming query because it gets called from outside the unit to return
+// .. information on the state of the unit
